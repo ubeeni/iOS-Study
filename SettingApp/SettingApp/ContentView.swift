5abe8e2fce5c5185129dd2ab184169cddabdd485
김유빈 <yubin6894@gmail.com>
2022-12-13 03:16:01 +0900

    셀 디테일 그리기 & 리팩토링

--- SettingApp/SettingApp/ContentView.swift
+++ SettingApp/SettingApp/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isAirplaneModeOn: Bool = false
+    
     var body: some View {
         
         NavigationView {
@@ -16,72 +19,27 @@ struct ContentView: View {
                     NavigationLink {
                         Text("프로필 화면")
                     } label: {
-                        HStack {
-                            Image("emoji2")
-                                .resizable()
-                                .aspectRatio( contentMode: .fit)
-                                .frame(width: 40, height: 40)
-                                .padding(.all, 10)
-                                .background(.white)
-                                .clipShape(Circle())
-                            
-                            VStack(alignment: .leading, spacing: 3) {
-                                Text("nini")
-                                    .font(.system(size: 24))
-                                    .fontWeight(.regular)
-                                Text("Apple ID, iCloud, 미디어 및 구입")
-                                    .font(.system(size: 14))
-                            }
-                            .padding(.leading, 6)
-                        }
-                        .padding(.vertical, 6)
+                        profileCell()
                     }
                 }
                 
                 Section {
-                    HStack {
-                        Image(systemName: "airplane")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.orange)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("에어플레인 모드") {
-                            Text("Safari 화면")
-                        }
+                    toggleCell(imageName: "airplane",
+                               iconColor: .orange,
+                               cellTitle: "에어플레인 모드",
+                               isOn: $isAirplaneModeOn)
+                    
+                    navigationLinkCell(imageName: "wifi",
+                                       iconColor: .blue,
+                                       cellTitle: "Wi-Fi",
+                                       subTitle: "U+NetD8A0_5G"){
+                        Text("Wi-Fi 화면")
                     }
                     
-                    HStack {
-                        Image(systemName: "wifi")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("Wi-Fi") {
-                            Text("Wi-Fi 화면")
-                        }
-                    }
-                
-                    HStack {
-                        Image(systemName: "antenna.radiowaves.left.and.right")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.green)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("셀룰러") {
-                            Text("셀룰러 화면")
-                        }
+                    navigationLinkCell(imageName: "antenna.radiowaves.left.and.right",
+                                       iconColor: .green,
+                                       cellTitle: "셀룰러"){
+                        Text("셀룰러 화면")
                     }
                 }
                     
@@ -89,7 +47,7 @@ struct ContentView: View {
                     HStack {
                         Image(systemName: "bell.badge.fill")
                             .resizable()
-                            .aspectRatio( contentMode: .fit)
+                            .aspectRatio(contentMode: .fit)
                             .frame(width: 18, height: 18)
                             .padding(.all,5)
                             .background(.red)
@@ -101,25 +59,16 @@ struct ContentView: View {
                         }
                     }
                     
-                    HStack {
-                        Image(systemName: "speaker.wave.3.fill")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.pink)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("사운드 및 햅틱") {
-                            Text("사운드 및 햅틱 화면")
-                        }
+                    navigationLinkCell(imageName: "speaker.wave.3.fill",
+                                       iconColor: .pink,
+                                       cellTitle: "사운드 및 햅틱"){
+                        Text("사운드 및 햅틱 화면")
                     }
                     
                     HStack {
                         Image(systemName: "moon.fill")
                             .resizable()
-                            .aspectRatio( contentMode: .fit)
+                            .aspectRatio(contentMode: .fit)
                             .frame(width: 14, height: 14)
                             .padding(.all,7)
                             .background(.indigo)
@@ -131,130 +80,126 @@ struct ContentView: View {
                         }
                     }
                     
-                    HStack {
-                        Image(systemName: "hourglass")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.indigo)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("스크린 타임") {
-                            Text("스크린 타임 화면")
-                        }
+                    navigationLinkCell(imageName: "hourglass",
+                                       iconColor: .indigo,
+                                       cellTitle: "스크린 타임"){
+                        Text("스크린 타임 화면")
                     }
                 }
                 
                 Section {
-                    HStack {
-                        Image(systemName: "gear")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.gray)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("일반") {
-                            Text("일반 화면")
-                        }
+                    navigationLinkCell(imageName: "gear",
+                                       iconColor: .gray,
+                                       cellTitle: "일반"){
+                        Text("일반 화면")
                     }
                     
-                    HStack {
-                        Image(systemName: "switch.2")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.gray)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("제어 센터") {
-                            Text("제어 센터 화면")
-                        }
+                    navigationLinkCell(imageName: "switch.2",
+                                       iconColor: .gray,
+                                       cellTitle: "제어 센터"){
+                        Text("제어 센터 화면")
                     }
                     
-                    HStack {
-                        Image(systemName: "textformat.size")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("디스플레이 및 밝기") {
-                            Text("디스플레이 및 밝기 화면")
-                        }
+                    navigationLinkCell(imageName: "textformat.size",
+                                       iconColor: .blue,
+                                       cellTitle: "디스플레이 및 밝기"){
+                        Text("디스플레이 및 밝기 화면")
                     }
                     
-                    HStack {
-                        Image(systemName: "faceid")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.green)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("Face ID 및 암호") {
-                            Text("Face ID 및 암호 화면")
-                        }
+                    navigationLinkCell(imageName: "faceid",
+                                       iconColor: .green,
+                                       cellTitle: "Face ID 및 암호"){
+                        Text("Face ID 및 암호 화면")
                     }
                     
-                    HStack {
-                        Image(systemName: "sos")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.red)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                
-                        NavigationLink("긴급 구조 요청") {
-                            Text("긴급 구조 요청 화면")
-                        }
+                    navigationLinkCell(imageName: "sos",
+                                       iconColor: .red,
+                                       cellTitle: "긴급 구조 요청"){
+                        Text("긴급 구조 요청 화면")
                     }
                     
-                    HStack {
-                        Image(systemName: "battery.100")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.green)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("배터리") {
-                            Text("배터리 화면")
-                        }
+                    navigationLinkCell(imageName: "battery.100",
+                                       iconColor: .green,
+                                       cellTitle: "배터리"){
+                        Text("배터리 화면")
                     }
                     
-                    HStack {
-                        Image(systemName: "hand.raised.fill")
-                            .resizable()
-                            .aspectRatio( contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all,4)
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(6)
-                        
-                        NavigationLink("개인정보 보호 및 보안") {
-                            Text("개인정보 보호 및 보안 화면")
-                        }
+                    navigationLinkCell(imageName: "hand.raised.fill",
+                                       iconColor: .blue,
+                                       cellTitle: "개인정보 보호 및 보안"){
+                        Text("개인정보 보호 및 보안 화면")
                     }
                 }
             }
             .navigationTitle(Text("설정"))
+        }
+    }
+    
+    @ViewBuilder
+    private func profileCell() -> some View {
+        HStack {
+            Image("emoji2")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+                .padding(.all, 10)
+                .background(.white)
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading, spacing: 3) {
+                Text("nini")
+                    .font(.system(size: 24))
+                    .fontWeight(.regular)
+                Text("Apple ID, iCloud, 미디어 및 구입")
+                    .font(.system(size: 14))
+            }
+            .padding(.leading, 6)
+        }
+        .padding(.vertical, 6)
+    }
+    
+    @ViewBuilder
+    private func iconImage(imageName: String, iconColor: Color) -> some View {
+        Image(systemName: imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 20, height: 20)
+            .padding(.all,4)
+            .background(iconColor)
+            .foregroundColor(.white)
+            .cornerRadius(6)
+    }
+    
+    
+    @ViewBuilder
+    private func toggleCell(imageName: String, iconColor: Color, cellTitle: String, isOn: Binding<Bool>) -> some View {
+        HStack {
+            iconImage(imageName: imageName, iconColor: iconColor)
+            
+            Toggle(cellTitle, isOn: $isAirplaneModeOn)
+        }
+    }
+    
+    @ViewBuilder
+    private func navigationLinkCell<V: View>(imageName: String, iconColor: Color, cellTitle: String, subTitle: String? = nil, destination: @escaping () -> V) -> some View {
+        HStack {
+            iconImage(imageName: imageName, iconColor: iconColor)
+            
+            if let subTitle = subTitle {
+                NavigationLink {
+                    destination()
+                } label: {
+                    HStack {
+                        Text(cellTitle)
+                        Spacer()
+                        Text(subTitle).foregroundColor(.gray)
+                    }
+                }
+            } else {
+                NavigationLink(cellTitle) {
+                    destination() //Text("~ 화면")
+                }
+            }
         }
     }
 }
